8171da6eea00860c674972f66adea868c2b42be7
abanoubemadfikry <93451704+abanoubemadfikry@users.noreply.github.com>
2023-02-07 18:11:06 +0200

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+using WebApplication2.Models;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication2.Startup))]
+namespace WebApplication2
+{
+    public partial class Startup
+    {
+        private ApplicationDbContext _context = new ApplicationDbContext();
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            CreateOrDefaultRolesAndUsers();
+        }
+        public void CreateOrDefaultRolesAndUsers()
+        {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(_context));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(_context));
+            IdentityRole role = new IdentityRole();
+            if (!roleManager.RoleExists("Admins"))
+            {
+                role.Name = "Admins";
+                roleManager.Create(role);
+                ApplicationUser user = new ApplicationUser();
+                user.UserName = "Abanoub";
+                user.Email = "pop47@gmail.com";
+                var check = userManager.Create(user, "qwe234A@");
+                if (check.Succeeded)
+                {
+                    userManager.AddToRole(user.Id, "Admins");
+                }
+            }
+        }
+    }
+}
